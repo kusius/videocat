23cee61282d1bbb3b2203b7e5125a4869a894f40
ISIH\georgeko <george.kousis@scytalys.com>
2024-08-07 10:37:50 +0300

    Fix windows setup script when multiple ffmpegs installed on the system

--- setup_windows.bat
+++ setup_windows.bat
@@ -6,9 +6,8 @@ echo Installing ffmpeg libraries
 winget install Gyan.FFmpeg.Shared
 
 echo Copying runtime libraries to PROJECT_BUILD_DIR
-pushd %LOCALAPPDATA%\Microsoft\WinGet\Packages
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`dir /s /b Gyan.FFmpeg.Shared* ^| dir /s /b /ad ffmpeg*`) DO (
+FOR /F "tokens=* USEBACKQ" %%F IN (`dir /s /b /ad %LOCALAPPDATA%\Microsoft\WinGet\Packages\ffmpeg*shared*`) DO (
 SET FFMPEG_DIR=%%F
 )
 
@@ -18,4 +17,3 @@ xcopy /d %FFMPEG_DIR%\bin\*.dll %PROJECT%\bin\windows
 
 echo Done.
 
-popd
